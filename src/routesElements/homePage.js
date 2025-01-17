a1dd0c6534d2e853360256bd4b5f573cbe34b661
Omid Tahmasebi <tomid7373@gmail.com>
2025-01-17 15:50:20 +0330

    commit (9)

--- src/routesElements/homePage.js
+++ src/routesElements/homePage.js
@@ -2,10 +2,74 @@ export const Home = ()=>{
     return(
         <div className="originContainer">
             <header className="originHeader">
-                <h2>IT News</h2>
+                <h2>IT NEWS</h2>
             </header>
 
             <main className="originMain">
+            <div>
+                    <h4>Top 5 digital transformation trends of 2025</h4>
+                    <p>
+                        The long-standing practice, which until recently was the focal point of business and technology change, 
+                        will find itself toiling in the background as AI developments move front and center. But at the same time, 
+                        digital transformation could spread across more practitioners than ever before.
+                        Front-line employees, equipped with AI-powered low-code tools, are expected to take on 
+                        more digitalization projects in a broad effort extending beyond the IT shop.
+                        The economics of digital transformations reveal another irony. Spending on transformation continues 
+                        to grow, with some market research estimates predicting a market approaching $4 trillion within a couple of years. 
+                        But an expanding market could also mean a maturing one. Some sectors such as e-commerce will likely show slower growth, 
+                        following a wave of rapid digital commerce adoption.
+                        Here are five digital transformation trends set to shape 2025 :<br/><br/>
+                        <b>1. AI supercharges digital transformation :</b> 
+                        AI-driven transformation emerged as a trend in 2023, as more organizations began to view generative AI (GenAI) 
+                        as a source of competitive advantage. That pattern will continue 
+                        in 2025 as AI as transformation becomes the rule rather than the exception. Expanding GenAI deployments and newer 
+                        agentic AI projects will play important roles in reinventing business processes. It will become increasingly difficult 
+                        to distinguish AI transformation from digital transformation.<br/><br/>
+                        <b>2. Digital transformation operates in the background : </b>
+                        For most of its history, digital transformation has been approached as a marquee project or an enterprise-wide initiative 
+                        made up of several projects.Business and technology forces, however, have compelled transformation to evolve. C-suite impatience 
+                        with the speed and cost of initiatives has reduced their size and scope in recent years. Emerging AI technologies, meanwhile, 
+                        have captured organizational attention and taken the spotlight from digital transformation. As a result, 
+                        IT leaders next year might see digital transformation become more of a general corporate directive rather than a specific, 
+                        high-profile initiative. That directive will point toward ROI-justified business outcomes, with technology providing the 
+                        means for getting there.<br/><br/>
+                        <b>3. Citizen developers take on transformation : </b>
+                        Digital transformation could also see a change in who does the work. Recent research suggests the responsibility 
+                        for digital transformation will be diffused across organizations, rather than concentrated in the IT department.
+                        Thomas Davenport and Ian Barkin, authors of the MIT Sloan School of Management article "How AI-empowered 
+                        'citizen developers' help drive digital transformation," contended that frustration with transformation 
+                        programs stems from businesses having too few centralized IT personnel to deliver digital capabilities.
+                        "Digital transformation is taking too long, in part because millions of application development, coding, 
+                        data science, and tech-associated jobs are going unfilled," according to the authors. "The only way for 
+                        companies to fill the void is through greater emphasis on the skill development of their existing 
+                        staff -- their citizens."<br/><br/>
+                        <b>4. Digital transformation matures : </b>4. Digital transformation matures : 
+                        While digital transformation remains a large and growing market, IT leaders might see signs of 
+                        maturation in 2025. That trend could manifest itself in a couple of ways.
+                        First, businesses will continue to carefully weigh transformation spending, a tendency that has 
+                        gained prominence since 2023 when enterprises began to emphasize cost optimization. CompTIA, an 
+                        IT industry association in Downers Grove, Ill., cited a higher bar for digital transformation as 
+                        its No. 1 trend to watch next year.
+                        "After heavy investments in digital tooling and resources after the pandemic, organizations will 
+                        be hitting the pause button to evaluate the success of past initiatives and to determine their 
+                        direction for the future," CompTIA noted in its "IT Industry Outlook 2025" report.<br/><br/>
+                        <b>5. Cloud migration continues as transformation focus : </b>
+                        Digital transformation over the years has often conflated with cloud adoption, at least until the 
+                        arrival of generative AI.
+                        It will become increasingly difficult to distinguish AI transformation from digital transformation.
+                        Cloud computing, however, continues as a transformation focus for many organizations. One of the reasons 
+                        is GenAI. Organizations outgrowing on-premises proofs of concept find space to grow in the cloud, 
+                        according to a December report from Wipro, a technology services and consulting company based in 
+                        Bengaluru, Kanataka, India.
+                        "As AI models have scaled up, our research shows that cloud has become a necessity for 9 out of 
+                        10 enterprises," the company's "Pulse of Cloud Quarterly Report" read.
+                        Enterprises will increasingly rely on both cloud and AI as enabling technologies for digital 
+                        transformation. But other factors will drive cloud transformation next year. Mainstream maintenance 
+                        for SAP's ERP Central Component software is set to expire at the end of 2027, making 2025 a potential 
+                        cloud migration milestone for businesses still using on-premises ERP.
+                    </p>
+                </div>
+
                 <div>
                     <h4>Will ChatGPT destroy coding ?</h4>
                     <p>
